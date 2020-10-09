1f0fc329289aa139f2ca45ab3c4452f62d19af7d
kdoh52 <kdoh52@gmail.com>
2020-10-08 19:11:14 -0700

    profile-setup.js

--- public/js/profile-setup.js
+++ public/js/profile-setup.js
@@ -1,1 +1,36 @@
 //When user clicks the "Creat Profile Button!" the user will be redirected to their profile
+
+$(document).ready(() => {
+
+    $(".profileSetup").submit(function(event){
+        event.preventDefault();
+
+        let dogProfile = {
+            petName: $("#petName").val().trim(),
+            petBreed: $("#petBreed").val().trim(),
+            petAge: $("#petAge").val().trim(),
+            petEnergy: $("#petEnergy").val().trim(),
+            petPersonality: $("#petPersonality").val().trim(),
+            petBio: $("#petBio").val().trim(),
+            petChip: $("#petChip").val().trim(),
+            petVet: $("#petVet").val().trim(),
+            petImg: $("#petImg").val().trim()
+        };
+
+        if(!dogProfile.petName || !dogProfile.petBreed || !dogProfile.petAge || !dogProfile.petEnergy || !dogProfile.petPersonality || !dogProfile.petBio || !dogProfile.petChip || !dogProfile.petVet) {
+            return;
+        }
+        console.log(dogProfile);
+        createProfile(dogProfile);
+    });
+
+    function createProfile(dogProfile) {
+        $.post("/api/createProfile", dogProfile)
+        .then(()=>{
+            window.location.replace("/dashboard");
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+    };
+});
