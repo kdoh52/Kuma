bb5148fcb34c964f398083b61c20a5a5459614f5
Alicia Gorton <apersigehl@gmail.com>
2020-10-09 22:02:02 -0700

    added missing ids to fix problem with submission form not correctly locating input elements

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,7 +1,9 @@
 $(document).ready(() => {
 
-    $(".signup").submit(function(event){
+    $("#signup-form").submit(function(event){
         event.preventDefault();
+
+        console.log('Signup form clicked');
 
         let newUser = {
             email: $("#email").val().trim(),
