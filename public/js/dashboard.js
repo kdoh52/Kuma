3a833a1df7a2ed18d20778b08ed3071014f40d6f
kdoh52 <kdoh52@gmail.com>
2020-10-14 20:28:23 -0700

    working dashboard/profile links

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,3 +1,29 @@
 $(document).ready(()=>{
-    
+    $('.dogName').on('click', function () {
+        console.log(this.id)
+        // $.get("/api/user_data").then((data) => {
+        //     console.log(`requesting /api/user_data with: ${JSON.stringify(data)}`);
+        //     return data.id;
+        // })
+        // .then(id => {
+        //     console.log(`redirecting user to /profile/${id}`);
+        //     window.location.replace("/profile/" + id);
+        // })
+        
+        window.location.replace("/profile/" + this.id);
+    });
+
+    $('.profileNav').on('click', function () {
+        // console.log("I AM HERE",this)
+        $.get("/api/user_data").then((data) => {
+            console.log(`requesting /api/user_data with: ${JSON.stringify(data)}`);
+            return data.id;
+        })
+        .then(id => {
+            console.log(`redirecting user to /profile/${id}`);
+            window.location.replace("/profile/" + id);
+        })
+        
+        // window.location.replace("/profile/" + this.id);
+    });
 });
--- public/js/profile.js
+++ public/js/profile.js
@@ -3,4 +3,18 @@ $(document).ready(() => {
       $(".member-name").text(data.email);
       $(".member-id").text(data.id);
     });
+
+    $('.profileNav').on('click', function () {
+      // console.log("I AM HEREEEEEEEEE")
+      $.get("/api/user_data").then((data) => {
+          console.log(`requesting /api/user_data with: ${JSON.stringify(data)}`);
+          return data.id;
+      })
+      .then(id => {
+          console.log(`redirecting user to /profile/${id}`);
+          window.location.replace("/profile/" + id);
+      })
+      
+      // window.location.replace("/profile/" + this.id);
+  });
 });
