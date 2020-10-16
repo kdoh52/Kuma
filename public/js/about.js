5c237aef930822b03dbff292a415fda33142a483
kdoh52 <kdoh52@gmail.com>
2020-10-15 17:23:59 -0700

    fixing styling/routes for new pages

---
+++ public/js/about.js
@@ -0,0 +1,15 @@
+$(document).ready(()=>{
+    $('.profileNav').on('click', function () {
+        // console.log("I AM HERE",this)
+        $.get("/api/user_data").then((data) => {
+            console.log(`requesting /api/user_data with: ${JSON.stringify(data)}`);
+            return data.id;
+        })
+        .then(id => {
+            console.log(`redirecting user to /profile/${id}`);
+            window.location.replace("/profile/" + id);
+        })
+        
+        // window.location.replace("/profile/" + this.id);
+    });
+});
